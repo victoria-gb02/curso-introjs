22c8e671dc0c84f535c6e0cc22d40714f22f3f59
Victoria Garcia Bustos <victoriagb@gmail.com>
2025-03-10 17:25:54 -0300

    Ejercicios Clase Práctica

--- file.js
+++ file.js
@@ -1,1 +1,9 @@
-// Add comment for example
+/* Más adelante vamos a querer validar que nuestros encuestados sean de 18 años de edad mínima y 99 años de edad máxima. 
+Nadie puede cambiar esas definiciones, por lo tanto, vamos a escribirlas en dos constantes (asignándoles los valores numéricos 18 y 99 respectivamente). Por último, deberás pedirle al usuario que ingrese su edad para validarla usando parseInt.
+Desglosando el ejercicio deberás:
+Crea una constante llamada EDAD_MINIMA y otra llamada EDAD_MAXIMA, a las cuales les asignamos los valores numéricos 18 y 99 respectivamente.
+Pide al usuario que ingrese su edad.
+Usa “parseInt” para convertir la entrada del usuario a un número entero.
+Valida si la edad ingresada está dentro del rango permitido y muestra un mensaje adecuado.
+Pista: Las constantes se declaran con la palabra reservada const y es buena práctica que sus nombres sean declarados siempre con letras mayúsculas. Utiliza parseInt para convertir la entrada del usuario a un número entero.
+ */
